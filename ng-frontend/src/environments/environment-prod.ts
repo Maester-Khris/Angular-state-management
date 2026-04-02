e94ca3c7f133da9ca89cf9143926ed21e7d882fa
Maester-khris <ndevjoel@gmail.com>
2026-04-02 00:49:54 -0400

    refactor(config): remove Python service credentials from frontend environment files

--- ng-frontend/src/environments/environment-prod.ts
+++ ng-frontend/src/environments/environment-prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: "https://angular-state-management.onrender.com", // Your Render Node URL
-  pythonBaseUrl: "https://angular-state-management-1.onrender.com", // Your Render Python URL
-  internalApiKey: 'ca0884183b821e7778598cddf017229ac742e4839b2bf114519bb3cf5d3bd280', // Your internal API key
   googleClientId: '554855208078-hk4802b9111gnlruvgipk2hejm8ngrh3.apps.googleusercontent.com',
 };
--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000', // Your local Node port
-  pythonBaseUrl: 'http://localhost:5000', // Your local Python port
-  internalApiKey: 'ca0884183b821e7778598cddf017229ac742e4839b2bf114519bb3cf5d3bd280', // Your internal API key
   googleClientId: '554855208078-hk4802b9111gnlruvgipk2hejm8ngrh3.apps.googleusercontent.com', // Your Google Client ID
 };
