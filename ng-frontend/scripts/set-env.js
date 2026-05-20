1a79e186145e3864b1ff336a5a343787f75901d3
Maester-khris <ndevjoel@gmail.com>
2026-05-20 02:04:32 -0400

    fix(ng-frontend): correct prod env filename to environment-prod.ts in set-env script

--- ng-frontend/scripts/set-env.js
+++ ng-frontend/scripts/set-env.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const prodEnvPath = './src/environments/environment.prod.ts';
+const prodEnvPath = './src/environments/environment-prod.ts';
 
 let content = fs.readFileSync(prodEnvPath, 'utf8');
 
@@ -10,6 +10,6 @@ content = content
 
 fs.writeFileSync(prodEnvPath, content);
 
-console.log('environment.prod.ts injected:');
+console.log('environment-prod.ts injected:');
 console.log('  NODE_SERVICE_URL:', process.env.NODE_SERVICE_URL);
 console.log('  GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID ? '***set***' : 'MISSING');
